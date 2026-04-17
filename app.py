297e9385d9c11215e38a42dab5df0477b1bd8989
OrAxelerator <axel.piat@icloud.com>
2026-04-17 09:53:50 +0200

    Add restart and quit option and some function in Snake class

--- app.py
+++ app.py
@@ -13,86 +13,11 @@ TICK = 0.1
 # ESCHAP = pause()
 
 def main(stdscr):
-    SNAKE = Snake(start=(4,0), direction=(0,1))
-    NB_APPLE = 7
-    APPLE = []
-    for i in range(NB_APPLE):
-        APPLE.append(Apple())
-    
-    # ===== INIT CURSES =====
-    curses.curs_set(0)
-    curses.start_color()
-    curses.init_pair(SNAKE_COLOR, curses.COLOR_GREEN, curses.COLOR_BLACK)
-    curses.init_pair(APPLE_COLOR, curses.COLOR_RED, curses.COLOR_BLACK)
-
-    stdscr.nodelay(True)
-    stdscr.keypad(True)
-
-    height, width = stdscr.getmaxyx()
-    win = curses.newwin(height - 2, width, 2, 0)
-    win.nodelay(True)
-    win.keypad(True)
-
-    side_bar = curses.newwin(3, width, 0, 0)
-    side_bar.clear()
-
-
-    
-
-    xp = 0
-    # apple = spawn_apple(win, SNAKE.snake_body)
-    for apple in APPLE:
-        apple.spawn_apple(SNAKE.snake_body, (height, width))
-
-    running = True
-
-    while running:
-        time.sleep(TICK)
-        key = stdscr.getch()
-        curses.set_escdelay(25)
-        if key == 27: 
-            stdscr.nodelay(False) 
-            while True:
-                time.sleep(1)
-                key = win.getch()
-                if key == 27: #ECHAP
-                    
-                    stdscr.nodelay(True)
-                    break
-        elif key in (ord('q'), ord('Q')):
+    while True:
+        result = game_loop(stdscr)
+        if result != "restart":
             break
-        elif key in (ord('r'), ord('R')):
-            pass
-        else:
-            pass
-
-
-        SNAKE.direction = update_direction(key, SNAKE.direction)
-
-        new_head = move_snake(SNAKE.snake_body[0], SNAKE.direction)
-
-        if collision(new_head, SNAKE.snake_body, win):
-            break
-
-        SNAKE.snake_body.insert(0, new_head)
-        eat_apple = False
-        for apple in APPLE:
-            if new_head == apple.pos:
-                apple.spawn_apple(SNAKE.snake_body, (height, width))
-                xp += 100
-                SNAKE.snake_body.insert(0, new_head) # to conter .pop
-
-
-        SNAKE.snake_body.pop()
-
-
-        render(win, SNAKE.snake_body, APPLE, side_bar, xp, SNAKE.direction)
-
-    game_over(stdscr)
-
-
-
-
+            
 
     
 
@@ -109,20 +34,6 @@ def update_direction(key, current):
     return current
 
 
-def move_snake(head, direction):
-    y, x = head
-    dy, dx = direction
-    return (y + dy, x + dx)
-
-
-def collision(head, snake, win):
-    h, w = win.getmaxyx()
-    y, x = head
-    if y <= 0 or y >= h - 1 or x <= 0 or x >= w - 1:
-        return True
-    if head in snake:
-        return True
-    return False
 
 
 def render(win, snake, apple, side, xp, debug):
@@ -143,9 +54,92 @@ def render(win, snake, apple, side, xp, debug):
 def game_over(stdscr):
     stdscr.nodelay(False)
     stdscr.clear()
-    stdscr.addstr(0, 0, "GAME OVER - Press any key [r] to restart")
+    stdscr.addstr(0, 0, "GAME OVER - Press [r] to restart or [q] to quit")
     stdscr.refresh()
-    stdscr.getch()
+
+    while True:
+        key = stdscr.getch()
+        if key in (ord('q'), ord('Q')):
+            return "quit"
+        elif key in (ord('r'), ord('R')):
+            return "restart"
     
+
+def game_loop(stdscr):
+        SNAKE = Snake(start=(4,0), direction=(0,1))
+        NB_APPLE = 3
+        APPLE = []
+        for i in range(NB_APPLE):
+            APPLE.append(Apple())
+        
+        # ===== INIT CURSES =====
+        curses.curs_set(0)
+        curses.start_color()
+        curses.init_pair(SNAKE_COLOR, curses.COLOR_GREEN, curses.COLOR_BLACK)
+        curses.init_pair(APPLE_COLOR, curses.COLOR_RED, curses.COLOR_BLACK)
+
+        stdscr.nodelay(True)
+        stdscr.keypad(True)
+
+        height, width = stdscr.getmaxyx()
+        win = curses.newwin(height - 2, width, 2, 0)
+        win.nodelay(True)
+        win.keypad(True)
+
+        side_bar = curses.newwin(3, width, 0, 0)
+        side_bar.clear()
+
+
+        
+
+        xp = 0
+        # spawn apple init
+        for apple in APPLE:
+            apple.spawn_apple(SNAKE.snake_body, (height, width))
+
+        running = True
+
+        while running:
+            time.sleep(TICK)
+            key = stdscr.getch()
+            curses.set_escdelay(25)
+            if key == 27: 
+                stdscr.nodelay(False) 
+                while True:
+                    time.sleep(1)
+                    key = win.getch()
+                    if key == 27: #ECHAP
+                        
+                        stdscr.nodelay(True)
+                        break
+            elif key in (ord('q'), ord('Q')):
+                break
+            elif key in (ord('r'), ord('R')):
+                pass
+            else:
+                pass
+
+
+            SNAKE.direction = update_direction(key, SNAKE.direction)
+
+            new_head = SNAKE.move()
+
+            if SNAKE.colision(new_head, (height, width)):
+                break
+
+            SNAKE.snake_body.insert(0, new_head)
+            for apple in APPLE:
+                if new_head == apple.pos:
+                    apple.spawn_apple(SNAKE.snake_body, (height, width))
+                    xp += 100
+                    SNAKE.snake_body.insert(0, new_head) # to conter .pop
+
+
+            SNAKE.snake_body.pop()
+
+
+            render(win, SNAKE.snake_body, APPLE, side_bar, xp, SNAKE.direction)
+
+        return game_over(stdscr)
 
 wrapper(main)
--- snake.py
+++ snake.py
@@ -1,10 +1,21 @@
-
-
 class Snake:
     def __init__(self, start: tuple, direction: tuple):
         self.start = start
         self.direction = direction
         self.snake_color = 1
         self.snake_body = [start]
+    
+    def move(self):
+        y, x = self.snake_body[0]
+        dy, dx = self.direction
+        return (y + dy, x + dx)
 
+    def colision(self, head, screenzise):
+        h, w = screenzise
+        y, x = head
+        if y <= 0 or y >= h - 1 or x <= 0 or x >= w - 1:
+            return True
+        if head in self.snake_body:
+            return True
+        return False
         
